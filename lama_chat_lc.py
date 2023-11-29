ef58b8dfff1e9cefc4d310d63ceda7020e3c689e
mahmoud abdella <62922536+mahmoud-ai@users.noreply.github.com>
2023-11-29 16:31:36 +0200

    Update lama_chat_lc.py

--- lama_chat_lc.py
+++ lama_chat_lc.py
@@ -2,4 +2,4 @@ from langchain.llms import HuggingFacePipeline
 
 llm = HuggingFacePipeline(pipeline=generate_text)
 
-print(llm(prompt="Explain to me the difference between nuclear fission and fusion."))
+print(llm(prompt="Explain to me the difference between Arabic union and Urobian union ?"))
