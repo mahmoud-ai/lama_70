c99fa298c96cc32b3be4391f3dad9323aedc19d1
mahmoud abdella <62922536+mahmoud-ai@users.noreply.github.com>
2023-12-03 21:21:25 +0200

    Update lama_chat_hf.py

--- lama_chat_hf.py
+++ lama_chat_hf.py
@@ -44,12 +44,12 @@ def create_dialogue(crisis, sector, is_injuries=0):
     #sector = translate_text(sector, "english")
 
     sector_messages = {
-        "health": f"It is imperative to consistently make decisions that safeguard the health of citizens, mitigate the risk of potential natural disasters, and offer recommendations aimed at preventing such occurrences. So, give me a short answer about {crisis} ",  # Health sector guidance
-        "national security": f"Your decisions should be geared towards ensuring Egyptian national security and the safety of the people, all while respecting international and diplomatic boundaries. So, give me a short answer about {crisis} ",  # National security sector guidance
-        "economic": f"You are required to make decisions that serve the best interests of the nation's economy, considering prevailing economic conditions, the stock market, and the guidance of the Central Bank of Egypt. Give a short answer about {crisis}",  # Economic sector guidance
-        "education": f" You are tasked with making decisions that prioritize the best interests of the students, fostering their learning experiences, and offering recommendations that contribute to the advancement of education. So, give me a short answer about {crisis} ",  # Education sector guidance
-        "foreign policy": f" The decision must align accurately with the policies and laws of the Arab Republic of Egypt, ensuring it is in the best interest of the country, all the while upholding strong international diplomatic relations. So, give me a short answer about {crisis} ",  # Foreign policy sector guidance
-        "media": f" Compliance with the regulations set forth by the unions associated with this sector is imperative, encompassing both audio-visual and written domains. So, give me a short answer about {crisis} "  # Media sector guidance
+        "health": f"It is imperative to consistently make decisions that safeguard the health of citizens, mitigate the risk of potential natural disasters, and offer recommendations aimed at preventing such occurrences.",  # Health sector guidance
+        "national security": f"Your decisions should be geared towards ensuring Egyptian national security and the safety of the people, all while respecting international and diplomatic boundaries. ",  # National security sector guidance
+        "economic": f"You are required to make decisions that serve the best interests of the nation's economy, considering prevailing economic conditions, the stock market, and the guidance of the Central Bank of Egypt.",  # Economic sector guidance
+        "education": f" You are tasked with making decisions that prioritize the best interests of the students, fostering their learning experiences, and offering recommendations that contribute to the advancement of education. ",  # Education sector guidance
+        "foreign policy": f" The decision must align accurately with the policies and laws of the Arab Republic of Egypt, ensuring it is in the best interest of the country, all the while upholding strong international diplomatic relations. ",  # Foreign policy sector guidance
+        "media": f" Compliance with the regulations set forth by the unions associated with this sector is imperative, encompassing both audio-visual and written domains. "  # Media sector guidance
     }
 
     prompt_base = f"As an Egyptian political man Please list the suggested actions from a {sector} perspective that a nation should take in response to a {crisis} , "
